1ef0ceb0361a5b741e356a452e8662b4757a2de1
Daniel Patterson <danieljpatterson77@gmail.com>
2018-08-15 15:42:46 +1000

    Add files via upload

---
+++ ajax-load.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("a.btn").click(function(){
+        $("#ajax-example").load("example.txt", function(responseTxt, statusTxt, xhr){
+            if(statusTxt == "success")
+                alert("External content loaded successfully!");
+            if(statusTxt == "error")
+                alert("Error: " + xhr.status + ": " + xhr.statusText);
+        });
+    });
+});
